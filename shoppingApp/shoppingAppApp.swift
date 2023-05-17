18678fa37ebb896114af02634f8e6594dd966779
Sarah Hill <sarah.hill4480@gmail.com>
2023-05-17 13:48:50 +0100

    Initial Commit

---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Hill, Sarah on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
