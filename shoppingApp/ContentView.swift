f00c37141f1b431f03a67bd9a8eee209bc9badbb
Sarah Hill <sarah.hill4480@gmail.com>
2023-05-18 13:41:09 +0100

    decoded JSON from dummyAPI [SH]

--- shoppingApp/ContentView.swift
+++ shoppingApp/ContentView.swift
@@ -7,20 +7,75 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Response: Codable {
+    var products: [Product]
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct Product: Codable {
+    let id: Int
+    let brand: String
+    let title: String
+    let description: String
+    let price: Float
+    let stock: Int
+    let images: [String]
+}
+
+
+
+struct ContentView: View {
+    
+    @State private var products = [Product]()
+    
+    
+    
+    var body: some View {
+        List (products, id: \.id) { item in
+            VStack(alignment: .leading){
+                Text(item.brand)
+                Text(item.title)
+                Text(item.description)
+                Text(String(item.price))
+                Text(String(item.stock))
+                
+                
+                
+            }
+        }
+        .task {
+            await loadData()
+        }
+        
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://dummyjson.com/products") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                products = decodedResponse.products
+            }
+        } catch (let error){
+            print(error)
+        }
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
